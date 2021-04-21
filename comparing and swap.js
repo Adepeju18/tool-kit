fc22790bc9138e0a636a6c6efc6d45fb20c4b209
Adepeju <pejulawson18@gmail.com>
2021-04-21 18:18:48 +0100

    adjust question 12

--- comparing and swap.js
+++ comparing and swap.js
@@ -1,11 +1,12 @@
-
+let swap1;
+let swap2;
 const scores = [73, 35, 68, 67, 20, 91, 42, 48, 55, 21, 95];
-for (let i = 1; i < scores.length; i++) {
+for (let i = 0; i < scores.length; i++) {
     if (scores[i] < scores[i + 1]) {
-        newScores = scores[i];
-        scores[i] = scores[i + 1];
-        scores[i + 1] = newScores;
-        i = +1;
+        swap1 = scores[i];
+        swap2 = scores[i + 1];
+        scores[i + 1] = swap2;
+        scores[i] = swap2;
 
     }
 }
